1165e994d76f462dbb63bfa8a8159e84602f90cf
bonnu <boxphere@cpan.org>
2009-02-05 16:50:04 +0900

    first commit

---
+++ lib/Class/Sasya.pm
@@ -0,0 +1,33 @@
+package Class::Sasya;
+
+use strict;
+use 5.8.1;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Class::Sasya -
+
+=head1 SYNOPSIS
+
+  use Class::Sasya;
+
+=head1 DESCRIPTION
+
+Class::Sasya is
+
+=head1 AUTHOR
+
+Satoshi Ohkubo E<lt>s.ohkubo@gmail.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
