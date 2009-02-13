ae82ddfdece2b09c6a74c212add229bbe616ccd8
bonnu <boxphere@cpan.org>
2009-02-13 20:13:53 +0900

    scope

---
+++ t/lib/Foo/Plugin/AnError.pm
@@ -0,0 +1,15 @@
+package Foo::Plugin::AnError;
+
+use strict;
+use warnings;
+use Class::Sasya::Plugin;
+
+hook_to 'main/phase2' => 'an_error';
+
+sub an_error {
+    die 'died on Foo::Plugin::AnError::an_error !!';
+}
+
+1;
+
+__END__
